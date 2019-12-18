d37f6c2352cfb059d961e7d344477a8e992e32b8
Tin Aung Yin <33687013+tinaungyin@users.noreply.github.com>
2019-12-19 00:21:17 +0630

    First Maven

---
+++ src/main/java/crackthecode/wtf/Producation/ProducationApplication.java
@@ -0,0 +1,13 @@
+package crackthecode.wtf.Producation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crackthecode/wtf/Producation/ProducationApplicationTests.java
@@ -0,0 +1,13 @@
+package crackthecode.wtf.Producation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
